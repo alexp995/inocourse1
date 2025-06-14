29c88e1b8789ad710cac5be9091444ebd174a45e
Aleksandr Pankrashin <alexp995@yandex.ru>
2025-06-14 20:49:18 +0300

    two change

--- src/main/java/inno/stepup/git.java
+++ src/main/java/inno/stepup/git.java
@@ -3,6 +3,6 @@ package inno.stepup;
 public class git {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("111");
+        System.out.println("11112");
     }
 }
