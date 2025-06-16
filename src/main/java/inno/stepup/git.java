cd9a09325f11966a247120817e173891afc4b3aa
Aleksandr Pankrashin <alexp995@yandex.ru>
2025-06-16 22:44:09 +0300

    feature2new

--- src/main/java/inno/stepup/git.java
+++ src/main/java/inno/stepup/git.java
@@ -4,6 +4,6 @@ public class git {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("111123456");
-        System.out.println("44455566");
+        System.out.println("44455566zzz");
     }
 }
